d215f1fc54b0f8f2e46785c00b7f560cb4af073e
Paul-Adrien Marie <pmarie@mozilla.com>
2024-08-05 10:21:20 +0200

    Comments added

--- static/federated.js
+++ static/federated.js
@@ -1,20 +1,40 @@
 import * as ort from "/dist/ort.training.wasm.min.js";
-import { loadJSON } from "./App.js";
 
 ort.env.wasm.wasmPaths = "/dist/";
 ort.env.wasm.numThreads = 1;
 
-let NUMIMAGES = 2;
+let NUMIMAGES = 5;
 let BATCHSIZE = 10;
 
-/** 
+/**
+ * Loads JSON from a given URL
+ * @async
+ * @load_Json
+ * @param {String} path - Path to the json file to load
+ * @returns {Promise<void>}
+ */
+async function loadJson(path) {
+  try {
+    const response = await fetch(path);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const config = await response.json();
+    return config;
+  } catch (error) {
+    console.error("Error loading config", error);
+  }
+}
+
+/**
  * Runs the federated learning scenario for a 100 users
  * @async
- * @returns{Promise<Void>}
-*/
+ * @runFederated
+ * @returns {Promise<Void>}
+ */
 export async function runFederated() {
   // Load the dataset to train on
-  let dataset = await loadJSON("/script/train.json");
+  let dataset = await loadJson("/script/train.json");
   // Get the length of the dataset
   const datasetLength = Object.keys(dataset).length;
   // Initialize the number of users
@@ -22,12 +42,12 @@ export async function runFederated() {
   // Initialize the number of completed user
   let completedUsers = 0;
 
-  /** 
+  /**
    * Runs the federated learning by batch of 20 users, 20 users run in parallel, in separate workers
    * @async
-   * @params {startIndex} Integer - Index to start  
-   * @returns{Promise<Void>}
-  */
+   * @param {Number} startIndex - Index to start the next batch of user
+   * @returns {Promise<Void>}
+   */
   async function runBatch(startIndex) {
     let promises = [];
     for (let j = 0; j < BATCHSIZE; j++) {
@@ -35,8 +55,8 @@ export async function runFederated() {
       if (userIndex >= numUsers) break;
       console.log(`Creating Worker for user ${userIndex + 1}`);
       let i = (userIndex * NUMIMAGES) % datasetLength;
-      let worker = new Worker('/script/worker.js', {
-        type:"module",
+      let worker = new Worker("/script/worker.js", {
+        type: "module",
       });
 
       // Send the data to the created Worker
@@ -78,9 +98,9 @@ export async function runFederated() {
     await runBatch(i);
   }
 
-  let completion_element = document.createElement('p');
+  let completion_element = document.createElement("p");
   completion_element.id = "completion_element_id";
   completion_element.innerText = "Process terminated";
-  let federated_button = document.getElementById('launch_federated');
+  let federated_button = document.getElementById("launch_federated");
   federated_button.appendChild(completion_element);
 }
